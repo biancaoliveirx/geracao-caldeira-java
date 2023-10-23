d5e5044bea214c9218473a28eaa76df597c50558
biancaoliveirx <134893856+biancaoliveirx@users.noreply.github.com>
2023-10-23 15:02:37 -0300

    Criando a classe domuneto, com atributos nome e numero de paginas

--- aula-05/src/ex05/Documento.java
+++ aula-05/src/ex05/Documento.java
@@ -1,4 +1,32 @@
 package ex05;
 
 public class Documento {
+    private String nome;
+    private int numeroPaginas;
+
+    public Documento(String nome, int numeroPaginas) {
+        this.nome = nome;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Documento " + "nome = " + nome + ", numero de páginas = " + numeroPaginas;
+    }
 }
