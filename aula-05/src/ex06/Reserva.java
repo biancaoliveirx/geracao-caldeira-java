1285646c0ae9181cf9dc53030fbdfa3fb5b24404
biancaoliveirx <134893856+biancaoliveirx@users.noreply.github.com>
2023-10-23 15:39:14 -0300

    Criando a classe Reserva com os atributos do exercício 06

--- aula-05/src/ex06/Reserva.java
+++ aula-05/src/ex06/Reserva.java
@@ -1,4 +1,30 @@
 package ex06;
 
 public class Reserva {
+    private String nome;
+    private String local;
+    private String cpf;
+    private String dataEntrada;
+    private String dataSaida;
+
+    public Reserva(String nome, String local, String cpf, String dataEntrada, String dataSaida) {
+        this.nome = nome;
+        this.local = local;
+        this.cpf = cpf;
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva: " + nome + ", local: " + local + "cpf: " + cpf + ", Data de Entrada: " + dataEntrada + ", Data de Saída: " + dataSaida;
+    }
 }
