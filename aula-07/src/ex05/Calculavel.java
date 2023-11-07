ecf3cfe3ca46210d1ac9a179ec0df701a5ab2f7f
biancaoliveirx <134893856+biancaoliveirx@users.noreply.github.com>
2023-11-07 11:22:56 -0300

    Criando Interface Calculavel

---
+++ aula-07/src/ex05/Calculavel.java
@@ -0,0 +1,8 @@
+package ex05;
+
+public interface Calculavel {
+    int adicionar(int a, int b);
+    int subtrair(int a, int b);
+    int multiplica(int a, int b);
+    int dividir(int a, int b) throws ArithmeticException;
+}
