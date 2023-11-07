ac757b6090e6d494d7a67e0fba4377d7d7e1ca7e
biancaoliveirx <134893856+biancaoliveirx@users.noreply.github.com>
2023-11-07 10:46:02 -0300

    Criando a classe Circulo

---
+++ aula-07/src/ex01/Circulo.java
@@ -0,0 +1,9 @@
+package ex01;
+
+public class Circulo implements Desenhavel {
+    @Override
+    public void desenhar() {
+        // Implementação do método desenhar para o Circulo
+        System.out.println("Vou desenhar o círculo.");
+    }
+}
